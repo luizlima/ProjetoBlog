88e790b9a8f3998349adacce3dfe37765500ac92
Luiz Lima <luizoscar.lima@gmail.com>
2017-11-01 19:51:53 -0200

    Criação do projeto de aplicação

---
+++ ProjetoBlog.Application/Interfaces/IAppService.cs
@@ -0,0 +1,17 @@
+﻿using ProjetoBlog.WebApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoBlog.Application.Interfaces
+{
+    public interface IAppService
+    {
+        void Add(PostViewModel postViewModel);
+        PostViewModel GetPost(string postId);
+        IEnumerable<PostViewModel> GetAll();
+        void Remove(string postId);
+    }
+}
---
+++ ProjetoBlog.Application/Services/PostAppService.cs
@@ -0,0 +1,49 @@
+﻿using ProjetoBlog.Application.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ProjetoBlog.WebApi.Models;
+using ProjetoBlog.Domain.Model;
+using ProjetoBlog.MongoDBAccess.Interfaces;
+
+namespace ProjetoBlog.Application.Services
+{
+    public class PostAppService : IAppService
+    {
+        private readonly IRepository<Post> _postRepository;
+
+        public PostAppService(IRepository<Post> postRepository)
+        {
+            _postRepository = postRepository;
+        }
+
+        public void Add(PostViewModel postViewModel)
+        {
+            Post post = new Post();
+            post.Title = postViewModel.Title;
+            post.Description = postViewModel.Description;
+            post.Url = postViewModel.Url;
+            post.PostBy = postViewModel.PostBy;
+            post.TimePosted = DateTime.Now;
+
+            _postRepository.Add(post);
+        }
+
+        public IEnumerable<PostViewModel> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public PostViewModel GetPost(string postId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(string postId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
